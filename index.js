626b7c83cd9ce9e5aeee08a9fcc58c644d754ac7
Ruan Nawe <ruannawe95@outlook.com>
2021-12-13 22:42:28 -0300

    Conexao mysql

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var restify = require('restify');
+const restify = require('restify');
 
 const server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
+});
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'root',
+    password : '',
+    database : 'db'
+  }
 });
 
 server.use(restify.plugins.acceptParser(server.acceptable));
